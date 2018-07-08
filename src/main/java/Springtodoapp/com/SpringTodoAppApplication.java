e61be402eed76f2dc503eff93961007fb7967d80
sakshi <sakshikansal08@gmail.com>
2018-07-08 12:26:32 +1000

    change in pom file

--- src/main/java/Springtodoapp/com/SpringTodoAppApplication.java
+++ src/main/java/Springtodoapp/com/SpringTodoAppApplication.java
@@ -3,16 +3,23 @@ package Springtodoapp.com;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 import Springtodoapp.com.data.UserRepository;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "Springtodoapp.com.data,Springtodoapp.com.config,Springtodoapp.com.model,Springtodoapp.com.web")
-public class SpringTodoAppApplication {
+public class SpringTodoAppApplication extends SpringBootServletInitializer {
 
 	@Autowired
 	static UserRepository userrepo;
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringTodoAppApplication.class);
+	}
 
 	public static void main(String[] args) {
 
